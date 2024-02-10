b97773b6f4ed210cc30d3a0eb3a8ac9e888fb1a6
Ali Seleem <engaliseleem@gmail.com>
2024-02-10 02:21:17 +0200

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="container">
       <h1 className='text-center'>About</h1>
       <p>
-        This simple project is bult as an application for react course by <a href="https://www.youtube.com/@codeZone">@codeZone</a>
+        This simple project is built as an application for react course by <a href="https://www.youtube.com/@codeZone">@codeZone</a>
       </p>
       <ol>
         I used:
